fae58d9c19c6599b9898c5e212625e1fc9734e8f
Филипп Баннов <bannov.2002@bk.ru>
2023-06-06 23:17:40 +0300

    Сообщение на почту

--- Electronic_goods/Electronic_goods/Pages/CheckoutPage.xaml.cs
+++ Electronic_goods/Electronic_goods/Pages/CheckoutPage.xaml.cs
@@ -1,7 +1,10 @@
-﻿using Electronic_goods.Models;
+﻿using Android.Net;
+using Electronic_goods.Models;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,6 +16,7 @@ namespace Electronic_goods.Pages
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class CheckoutPage : ContentPage
     {
+        Tovars tovars;
         public CheckoutPage(int a, int b)
         {
             InitializeComponent();
@@ -21,6 +25,7 @@ namespace Electronic_goods.Pages
             SumLbl1.Text = $"{b} P";
             FIOEntry.Text = $"{App.client.Surname} {App.client.Name} {App.client.Patronymic}";
             BindingContext = App.client;
+            
         }
 
         private async void TapGestureRecognizer_Tapped(object sender, EventArgs e)
@@ -29,8 +34,32 @@ namespace Electronic_goods.Pages
         }
         private async void OrderBtn_Clicked(object sender, EventArgs e)
         {
-            await DisplayAlert("Уведомление", "Заказ успешно оформлен", "ОК");
-            await Navigation.PopAsync();
+            try
+            {
+                MailAddress fromAdress = new MailAddress($"bannov.2002@bk.ru", "Electronick_Goods");
+                MailAddress toAdress = new MailAddress(mailEnt.Text, Login.Text);
+                MailMessage message = new MailMessage(fromAdress, toAdress);
+                message.Body = "Магазин Electronick_Goods приветствует вас," + Environment.NewLine + $"{FIOEntry.Text}" + Environment.NewLine + "Спасибо вам за покупку в нашем магазине, мы сообщим когда ваш товар прибудет!!!";
+                message.Subject = "Администрация магазина - Electronick_Goods";
+
+                SmtpClient smtp = new SmtpClient();
+                smtp.Host = "smtp.mail.ru";
+                smtp.Port = 587;
+                smtp.EnableSsl = true;
+                smtp.DeliveryMethod = SmtpDeliveryMethod.Network;
+                smtp.UseDefaultCredentials = false;
+                smtp.Credentials = new NetworkCredential(fromAdress.Address, "CbET0LjDiduaeLPpzYgZ");
+
+                smtp.Send(message);
+
+                await DisplayAlert("Уведомление", "Заказ успешно оформлен", "ОК");
+                await Navigation.PopAsync();
+            }
+            catch (Exception ex)
+            {
+
+                await DisplayAlert("Уведомление", ex.Message.ToString(), "ОК");
+            }
         }
     }
 }
